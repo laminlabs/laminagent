29bb958ed0d947f219e6b39e30478d025d74631e
Alex Wolf <f.alexander.wolf@gmail.com>
2026-06-16 01:09:49 +0200

    👷 Connect to `lamindata` for metric tracking during push events on main (#20)

--- tests/examples/conftest.py
+++ tests/examples/conftest.py
@@ -1,3 +1,4 @@
+import os
 import shutil
 from pathlib import Path
 
@@ -8,6 +9,16 @@ from testutils import TESTDB1_DEV_DIR, TESTDB1_NAME, TESTDB1_STORAGE
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_testdb1():
+    # for metric tracking, we're logging runs against main
+    # to the lamindata instance
+    if (
+        os.getenv("GITHUB_ACTIONS") == "true"
+        and os.getenv("GITHUB_EVENT_NAME") == "push"
+        and os.getenv("GITHUB_REF") == "refs/heads/main"
+    ):
+        ln.connect("laminlabs/lamindata")
+        return
+
     dbroot = Path(TESTDB1_STORAGE)
     if dbroot.exists():
         print("removing existing testdb1 storage location and database")
