f4ddeb90063c4a49cb8296324da26d4ae62e8c17
Alex Wolf <f.alexander.wolf@gmail.com>
2026-06-16 01:13:47 +0200

    💚 Fix run on main

--- tests/examples/conftest.py
+++ tests/examples/conftest.py
@@ -9,6 +9,9 @@ from testutils import TESTDB1_DEV_DIR, TESTDB1_NAME, TESTDB1_STORAGE
 
 @pytest.fixture(scope="session", autouse=True)
 def setup_testdb1():
+    dev_dir = Path(TESTDB1_DEV_DIR)
+    dev_dir.mkdir(parents=True, exist_ok=True)
+
     # for metric tracking, we're logging runs against main
     # to the lamindata instance
     if (
@@ -17,6 +20,7 @@ def setup_testdb1():
         and os.getenv("GITHUB_REF") == "refs/heads/main"
     ):
         ln.connect("laminlabs/lamindata")
+        ln.setup.settings.dev_dir = dev_dir
         return
 
     dbroot = Path(TESTDB1_STORAGE)
@@ -24,7 +28,6 @@ def setup_testdb1():
         print("removing existing testdb1 storage location and database")
         shutil.rmtree(dbroot)
         ln.setup.delete(TESTDB1_NAME, force=True)
-    dev_dir = Path(TESTDB1_DEV_DIR)
     if dev_dir.exists():
         print("removing existing testdb1 development directory")
         shutil.rmtree(dev_dir)
