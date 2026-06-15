0992c1c76253f7d6001384bbc7504e9454999233
Alex Wolf <f.alexander.wolf@gmail.com>
2026-06-16 01:24:12 +0200

    💚 Fix

--- tests/examples/conftest.py
+++ tests/examples/conftest.py
@@ -10,6 +10,9 @@ from testutils import TESTDB1_DEV_DIR, TESTDB1_NAME, TESTDB1_STORAGE
 @pytest.fixture(scope="session", autouse=True)
 def setup_testdb1():
     dev_dir = Path(TESTDB1_DEV_DIR)
+    if dev_dir.exists():
+        print("removing existing testdb1 development directory")
+        shutil.rmtree(dev_dir)
     dev_dir.mkdir(parents=True, exist_ok=True)
 
     # for metric tracking, we're logging runs against main
@@ -19,18 +22,13 @@ def setup_testdb1():
         and os.getenv("GITHUB_EVENT_NAME") == "push"
         and os.getenv("GITHUB_REF") == "refs/heads/main"
     ):
-        ln.connect("laminlabs/lamindata")
         ln.setup.settings.dev_dir = dev_dir
         return
 
-    dbroot = Path(TESTDB1_STORAGE)
-    if dbroot.exists():
+    storage_root = Path(TESTDB1_STORAGE)
+    if storage_root.exists():
         print("removing existing testdb1 storage location and database")
-        shutil.rmtree(dbroot)
+        shutil.rmtree(storage_root)
         ln.setup.delete(TESTDB1_NAME, force=True)
-    if dev_dir.exists():
-        print("removing existing testdb1 development directory")
-        shutil.rmtree(dev_dir)
-    dev_dir.mkdir(parents=True, exist_ok=True)
-    ln.setup.init(name=TESTDB1_NAME, storage=dbroot, modules="bionty")
+    ln.setup.init(name=TESTDB1_NAME, storage=storage_root, modules="bionty")
     ln.setup.settings.dev_dir = dev_dir
